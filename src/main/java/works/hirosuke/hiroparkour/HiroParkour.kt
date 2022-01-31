ae87fb4f6bd03e4c215042496a7bde792fca8db6
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-01 02:43:37 +0900

    Initial commit

---
+++ src/main/java/works/hirosuke/hiroparkour/HiroParkour.kt
@@ -0,0 +1,13 @@
+package works.hirosuke.hiroparkour
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HiroParkour : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
